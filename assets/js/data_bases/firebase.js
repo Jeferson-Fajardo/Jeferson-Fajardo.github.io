c9afbcdacd935a928bb099240328f86677f8271f
Gourmt <jefersonleandrorodriguez@gmail.com>
2025-06-13 13:40:23 -0500

    fix: Add .js to the URL in env.public.js.

--- assets/js/data_bases/firebase.js
+++ assets/js/data_bases/firebase.js
@@ -12,7 +12,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { firebaseConfig } from "../env.public";
+import { firebaseConfig } from "../env.public.js";
 // Load the firebase configuration from the config.json file
 async function loadFirebaseConfig() {
  
